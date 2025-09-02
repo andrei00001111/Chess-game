7e99a3eab43db4d261aaf8e4b551a2b7d246d0ba
Andrei Paduraru <andrei1.paduraru@gmail.com>
2025-09-02 12:16:31 +0200

    Added move related methods

--- ChessGame.py
+++ ChessGame.py
@@ -1,6 +1,13 @@
 import Board
 import Side
+import Square
 import Player
+from Pieces import Pawn
+from Pieces import Queen
+from Pieces import King
+from Pieces import Rook
+from Pieces import Knight
+from Pieces import Bishop
 class ChessGame:
     def __init__(self):
         self.gameBoard = Board()
@@ -8,26 +15,82 @@ class ChessGame:
         self.blackPoints = 0
         self.moves = []
         
-    def makeMove(self, move, side:Side):
-        isCastles = self.isMoveCastles(move)
-        player = self.getPlayerFromMove(move)
-        if(isCastles):
-            self.gameBoard.makeCastle(move, side)
-            return
-        pieceType = self.getTypeFromMove(move)
-        isCapture = self.isMoveCapture(move)
-        inputSquare = self.getInputFromMove(move)
-        outputSquare = self.getOutputFromMove(move)
-
-        if isCapture:
-            
-            player.addPoints()
+    def analyzeMove(self, move:str, side:Side):
         
-        self.gameBoard.putPieceInSquare(pieceType, outputSquare)
-        self.gameBoard.putPieceInSquare()
-        self.moves.append(move)
+        # Get Castles, capture
+        if(move.find('O-O-O')):
+            self.makeLongCastles(side)
+        elif (move.find('O-O')):
+            self.makeShortCastles(side)
+        elif (move.find('x')):
+            capture = True
+        
+        inputData = [0,0]
+        outputData = [0,0]
+        # pawn move
+        if (move[0].islower):
+            if(capture):
+                outputData = self.moveToPos(move)
+                inputData = outputData
+                inputData[1] = inputData[1] - 1
+                inputData[0] = ord(move[0])
+            else:
+                outputData = self.moveToPos(move[-2:])
+                inputData = outputData
+                inputData = outputData[1] - 1
+        # Else normal move 
+        else:
+            outputMove = self.moveToPos(move)
+    def makeInitPosition(self):
+        for i in range(0,8):
+            whitePawn = Pawn('White')
+            blackPawn = Pawn('Black')
+            self.gameBoard.putPieceInSquare(whitePawn, 1, i)
+            self.gameBoard.putPieceInSquare(blackPawn, 6, i)
+        
+        whiteQueen = Queen('White')
+        blackQueen = Queen('Black')
+        self.gameBoard.putPieceInSquare(whiteQueen, 0, 3)
+        self.gameBoard.putPieceInSquare(blackQueen, 7, 3)
+        
+        whiteKing = King('White')
+        blackKing = King('Black')
+        self.gameBoard.putPieceInSquare(whiteKing, 0, 4)
+        self.gameBoard.putPieceInSquare(blackKing, 7, 4)
 
-    def getPieceTypeFromMove(self,move):
+        for i in range (2):
+            whiteKnight = Knight('White')
+            blackKnight = Knight('Black')
+            
+            whiteRook = Rook('White')
+            blackRook = Rook('Black')
+
+            whiteBishop = Bishop('White')
+            blackBishop = Bishop('Black')
+
+            if i == 0:
+                self.gameBoard.putPieceInSquare(whiteRook, 0, 0)
+                self.gameBoard.putPieceInSquare(blackRook, 7, 0)
+
+                self.gameBoard.putPieceInSquare(whiteKnight, 0, 1)
+                self.gameBoard.putPieceInSquare(blackKnight, 7, 1)
+
+                self.gameBoard.putPieceInSquare(whiteBishop, 0, 2)
+                self.gameBoard.putPieceInSquare(blackBishop, 7, 2)
+
+            elif i == 1:
+                self.gameBoard.putPieceInSquare(whiteRook, 0, 7)
+                self.gameBoard.putPieceInSquare(blackRook, 7, 7)
+
+                self.gameBoard.putPieceInSquare(whiteKnight, 0, 6)
+                self.gameBoard.putPieceInSquare(blackKnight, 7, 6)
+
+                self.gameBoard.putPieceInSquare(whiteBishop, 0, 5)
+                self.gameBoard.putPieceInSquare(blackBishop, 7, 5)
+
+
+
+    def getInitSquare(self,move):
         pass
 
     def inputGameFromFile(self, gamePath):
@@ -38,3 +101,14 @@ class ChessGame:
     
     def restartGame (self):
         pass
+    
+    def moveToPos(self, data:str):
+        if (data.find('+')):
+            data = data[-2:]
+            data = data[0:2]
+        else: data = data[-2:]
+        data1 = data[0:1]
+        data2 = data[1:2]
+
+        letter = ord(data1) - ord('a')
+        return [letter, int(data2)]
